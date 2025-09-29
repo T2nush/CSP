e66cff1c87d1c721bce137549d0da7b44a36ee83
T2nush <236013@shakopeeschools.org>
2025-09-29 09:42:49 -0500

    Update 119_Tanush_Algorithms_ART.py
    Figuring out drawing galaxy part

--- 1.1.9 - Algorithms and Art/119_Tanush_Algorithms_ART.py
+++ 1.1.9 - Algorithms and Art/119_Tanush_Algorithms_ART.py
@@ -6,20 +6,29 @@ screen = turtle.Screen()
 screen.bgcolor("black")
 screen.title("Large Galaxy Spiral")
 
+#---------------------------------------------------------
 # Turtle Custom create
 galaxy = turtle.Turtle()
 galaxy.shape("turtle")
 galaxy.speed(0)  
 
+
+#---------------------------------------------------------
 #Color creater
 colors = ["white", "yellow", "lightblue", "violet", "pink"]
 
-# Custom function: draw a star
+
+#---------------------------------------------------------
+# My crteating Custom function: tell to draw a star
 def draw_star(size, color):
     galaxy.fillcolor(color)
     galaxy.begin_fill()
     for _ in range(5):
         galaxy.forward(size)
-        galaxy.right(144)  # Star angle ask meinhart
+        galaxy.right(144)  # Star angle ask mainhardt
     galaxy.end_fill()
 
+
+#---------------------------------------------------------
+#User question/input part
+arms = int(screen.numinput("galaxy setting", "How many arms for the spiral", 5,2,10))
