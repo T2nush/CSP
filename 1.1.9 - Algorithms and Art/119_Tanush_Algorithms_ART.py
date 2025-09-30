43b902c445e476a635f99c76815b07c8332c6b0b
T2nush <236013@shakopeeschools.org>
2025-09-29 23:20:53 -0500

    Update 119_Tanush_Algorithms_ART.py
    Fixed angles, have to ask bout one, finished user input and coordinates dont with papor

--- 1.1.9 - Algorithms and Art/119_Tanush_Algorithms_ART.py
+++ 1.1.9 - Algorithms and Art/119_Tanush_Algorithms_ART.py
@@ -1,34 +1,45 @@
 import turtle
 import random
 
+#Create Turtle
+star_shape = (
+(0, 0),
+(10, 40),
+(20, 0),
+ (-10, 25),
+(30, 25),
+)
+
+
 # Seting up the screen
 screen = turtle.Screen()
 screen.bgcolor("black")
 screen.title("Large Galaxy Spiral")
 
 #---------------------------------------------------------
-# Turtle Custom create
-galaxy = turtle.Turtle()
-galaxy.shape("turtle")
-galaxy.speed(0)  
 
 
 #---------------------------------------------------------
-#Color creater
+#Color creator
 colors = ["white", "yellow", "lightblue", "violet", "pink"]
 
 
 #---------------------------------------------------------
-# My crteating Custom function: tell to draw a star
+# My crteating Custom function, tell to draw a star
 def draw_star(size, color):
     galaxy.fillcolor(color)
-    galaxy.begin_fill()
+    galaxy.begin_fill() 
     for _ in range(5):
         galaxy.forward(size)
-        galaxy.right(144)  # Star angle ask mainhardt
+        galaxy.right(145)  # Star angle ask mainhardt
     galaxy.end_fill()
 
 
 #---------------------------------------------------------
 #User question/input part
 arms = int(screen.numinput("galaxy setting", "How many arms for the spiral", 5,2,10))
+stars_for_each_per_arm = int (screen.numinput)
+
+
+
+#Galaxy Drawing 
