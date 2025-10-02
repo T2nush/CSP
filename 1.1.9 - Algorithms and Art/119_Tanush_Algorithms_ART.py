f0a7a12484c6f8ef1ee6e1475c77a8cf9a5d0983
T2nush <236013@shakopeeschools.org>
2025-10-01 22:53:30 -0500

    Update 119_Tanush_Algorithms_ART.py

--- 1.1.9 - Algorithms and Art/119_Tanush_Algorithms_ART.py
+++ 1.1.9 - Algorithms and Art/119_Tanush_Algorithms_ART.py
@@ -1,31 +1,35 @@
-import turtle
+import turtle 
 import random
 
 #Create Turtle
 star_shape = (
-(0, 0),
-(10, 40),
-(20, 0),
- (-10, 25),
-(30, 25),
+    (0, 0),
+    (10, 40),
+    (20, 0),
+    (-10, 25),
+    (30, 25),
 )
+turtle.register_shape("star", star_shape)
 
-
+#---------------------------------------------------------
 # Seting up the screen
 screen = turtle.Screen()
 screen.bgcolor("black")
 screen.title("Large Galaxy Spiral")
 
 #---------------------------------------------------------
-
+#TURTLE Create
+galaxy = turtle.Turtle()
+galaxy.shape("star")
+galaxy.speed(0)
 
 #---------------------------------------------------------
 #Color creator
-colors = ["white", "yellow", "lightblue", "violet", "pink"]
+colors = ["white", "yellow", "blue", "red", "pink"]
 
 
 #---------------------------------------------------------
-# My crteating Custom function, tell to draw a star
+# My creating Custom function, tell to draw a star
 def draw_star(size, color):
     galaxy.fillcolor(color)
     galaxy.begin_fill() 
@@ -37,9 +41,26 @@ def draw_star(size, color):
 
 #---------------------------------------------------------
 #User question/input part
-arms = int(screen.numinput("galaxy setting", "How many arms for the spiral", 5,2,10))
-stars_for_each_per_arm = int (screen.numinput)
+arms = int(screen.numinput("Galaxy Setting", "How many arms for the spiral", 5,2,12))
+stars_for_each_per_arm = int(screen.numinput("Galaxy Settings", "How many stars for each arm?", 20, 5, 400))
 
-
-
+#---------------------------------------------------------
 #Galaxy Drawing 
+for arm in range(arms):
+    galaxy.penup()
+    galaxy.home()
+    galaxy.right((360 / arms) * arm)
+    for star in range(stars_for_each_per_arm):
+        galaxy.forward(10 + star * 5)
+        color = random.choice(colors)
+        size = random.randint(5,15)
+        draw_star(size,color)
+
+galaxy.penup()
+galaxy.goto(-250,-250)
+galaxy.color("white")
+galaxy.write("67", align="center", font=("Arial", 16, "bold"))
+
+galaxy.hideturtle()
+screen.mainloop()
+    
