3c1006ac25cb7ba95a67ec693afda41cc5231b1e
T2nush <236013@shakopeeschools.org>
2025-10-31 08:56:08 -0500

    body and hang start varaibales finish
    ...

--- 1.2.5 - Shall We Play a Game/125_Tanush.py
+++ 1.2.5 - Shall We Play a Game/125_Tanush.py
@@ -1,6 +1,6 @@
-#Start screen asking for difficulty, (Only options are hard, and extremely hard) finish by OCT 29
-# TODO #Setting up word, draw short line for each letter, each line has a letter the user guessed on top of it. Lives represented with head, body, left arm, right arm, left leg, right leg, and powerups if you run out to give a free letter. Finish by oct 29 also.
-Set up extra feature, each guess user has, they can guess a letter and a number, numbers are randomized within the random word which is going to be 8-13 letters long. The words will be difficult to guess and long due to a feature Im adding giving the user a extra life/cutting off one of the body parts on the hangman if they guess the letter with the correct number below it (guessing). Finish by 30 ALSO, create word stuff.
+#TODO Start screen asking for difficulty, (Only options are hard, and extremely hard) finish by OCT 29
+#TODO #Setting up word, draw short line for each letter, each line has a letter the user guessed on top of it. Lives represented with head, body, left arm, right arm, left leg, right leg, and powerups if you run out to give a free letter. Finish by oct 29 also.
+#TODO Set up extra feature, each guess user has, they can guess a letter and a number, numbers are randomized within the random word which is going to be 8-13 letters long. The words will be difficult to guess and long due to a feature Im adding giving the user a extra life/cutting off one of the body parts on the hangman if they guess the letter with the correct number below it (guessing). Finish by 30 ALSO, create word stuff.
 # TODO #Create Superpower (only 1 option), gives back life if you are on your last life/right leg. Finish by oct 31
 # TODO If all body parts are there, show lose screen Nov 1st or 2nd
 # TODO If word guessed, show win screen nov 1st-3rd
@@ -18,7 +18,7 @@ Word_list = [ "LIGHTHOUSE", "RESPONSIBILITY", "UNDERGROUND", "CONSTITUTION", "AS
 MIN_WORD_LEN = 8
 
 #screen set
-SCREEN_WIDTH = 1000
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 #---------------------------------------------
@@ -29,4 +29,52 @@ MAX_WRONG = len(HANGMAN_PARTS)
 #---------------------------------------------
 # Turtle setup area
 screen = turtle.screen()
-screen = 
+screen.setup(SCREEN_WIDTH, SCREEN_HEIGHT)
+screen.bgcolor("lightblue")
+screen.title("Hangman w/ Numbers and Powers")
+
+
+pen = turtle.Turtle()
+pen.hideturtle()
+pen.speed = (0)
+pen.size(3)
+
+#---------------------------------------------
+#Variables for game
+difficulty = None
+word = ""
+word_letters = []
+guessed = []
+
+#---------------------------------------------
+#Drawing the hannger and functions
+'''def draw_text(x, y, text, size=25, color="black", align="center"):
+    pen.penup()
+    pen.goto (x,y)
+    pen.color(color)
+    pen.write(text, align=align, font=("Arial" , size, "bold" ))
+'''
+#hanger (get from google docs)
+def draw_hanger():
+    pen.penup()
+    pen.goto(-150, -150)
+    pen.pendown()
+    pen.forward(100)  # base
+    pen.backward(50)
+    pen.left(90)
+    pen.forward(200)  # pole
+    pen.right(90)
+    pen.forward(75)   # top part
+    pen.right(90)
+    pen.forward(25)    # rope
+    pen.penup()
+    pen.home()
+
+ #---------------------------------------------
+  
+
+def draw_body_parts (part):
+change.pensize(2)
+
+
+
