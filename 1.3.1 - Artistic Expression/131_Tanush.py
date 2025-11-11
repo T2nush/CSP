036f440f2c319eaf0702ac363644e6082b29ddc2
T2nush <236013@shakopeeschools.org>
2025-11-11 09:43:02 -0600

    screen 1
    x

---
+++ 1.3.1 - Artistic Expression/131_Tanush.py
@@ -0,0 +1,39 @@
+# TODO
+# TODO
+# TODO 
+# TODO
+# TODO
+# TODO
+
+
+
+import turtle as turt
+wn = turt.Screen()
+
+#Letter part 
+wn.screen.textinput("Click the Envelope!")
+wn.setup(width=600, height=600)
+normal_letter = "blueletter.gif"
+wn.addshape(normal_letter)
+turtle_letter = turt.Turtle()
+turtle_letter.shape(normal_letter)
+def go_blue(x,y):
+    wn.bgcolor("LightBlue")
+    turtle_letter.hideturtle()
+turtle_letter.onclick(go_blue)
+#-------------------------------------------------------
+# Text input/starting questions (name, age
+
+
+
+
+wn.mainloop()
+
+
+
+
+
+
+
+
+
